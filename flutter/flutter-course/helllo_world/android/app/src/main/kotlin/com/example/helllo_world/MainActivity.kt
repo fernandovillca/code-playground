17edc537c30777b9f1537da017cdb008aa16a7c1
Fernando <vluisfernando62@gmail.com>
2025-12-19 15:26:51 -0400

    feat: add basic Flutter project

---
+++ flutter/flutter-course/helllo_world/android/app/src/main/kotlin/com/example/helllo_world/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.helllo_world
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter/flutter-course/helllo_world/lib/presentation/screens/counter_screen.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class CounterScreen extends StatefulWidget {
+  const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+  int clickCounter = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              '$clickCounter',
+              style: TextStyle(fontSize: 100, fontWeight: FontWeight.w100),
+            ),
+            Text('Cantidad de clicks'),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+        },
+        child: Icon(Icons.plus_one),
+      ),
+    );
+  }
+}
